a4c055d87afed7ae5a161eb4c92ce3c046926298
root <root@bfddbeddcbecbccfaabacdbdbeaebdaae-0.bfddbeddcbecbccfaabacdbdbeaebdaae.che.svc.cluster.local>
2023-03-27 04:11:53 +0000

    Backup Branch before deleting

---
+++ jspservlet-app-ATM/src/main/java/service/InterfaceClass.java
@@ -0,0 +1,7 @@
+package service;
+
+interface InterfaceClass {
+	abstract public double withdrawAmount(double arg1,double arg2);
+      abstract public double depositeAmount(double arg1, double arg2);
+      abstract public double accountBalance(double arg1);
+}
